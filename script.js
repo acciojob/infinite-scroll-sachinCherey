db065ebd203cc31a0ed7020bd9d1cb57ac6bf1a3
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-08-25 11:58:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here!
+document.addEventListener('DOMContentLoaded',function()){
+const itemList=document.getElementById('infi-list');
+function addItem() {
+    const newItem = document.createElement("li");
+    newItem.textContent = "Item " + (itemList.children.length + 1);
+    itemList.appendChild(newItem);
+  }
+window.addEventListener("scroll", function () {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      addItem();
+      addItem();
+    }
+  });
 
